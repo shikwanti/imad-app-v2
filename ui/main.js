d2802c2fb22a17bd263f5091232f5e22c5aeb680
inderjeet kaur khokhar <shikwanti@gmail.com>
2017-02-14 00:10:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 
 
 //change the main text div
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
+var element = document.getElementById('main-text');
+element.innerHTML='New-Value';
