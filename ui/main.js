72419e7a013ee58966d0345360dd27ee7e5c77de
inderjeet kaur khokhar <shikwanti@gmail.com>
2017-02-14 22:57:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,14 @@ var element = document.getElementById('main-text');
 element.innerHTML='New-Value';
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function()
 {
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
 };
+    
