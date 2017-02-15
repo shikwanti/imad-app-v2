23bbf0a0f7bdf50175f85919f01749bddb1428be
inderjeet kaur khokhar <shikwanti@gmail.com>
2017-02-15 18:55:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,3 @@
 console.log('Loaded!');
 
 
-
-//move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,20);
-};
-    
