7d9ce06db9e1431fa9442c52525267f5064bb69b
inderjeet kaur khokhar <shikwanti@gmail.com>
2017-03-21 23:47:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
-
+var config ={
+    user : 'shikwanti',
+    host:'db.imad.hasura-app.io',
+    database:'shikwanti',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var articles = {
      'article-one' :
         {
@@ -109,7 +115,19 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
-
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    //return a response with the results
+pool.query('SELECT * FROM test',function(err,result){
+   if(err)
+   {
+       res.status(500).send(err,toString());
+   }
+   else
+   res.send(JSON.stringify(result));
+});
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
