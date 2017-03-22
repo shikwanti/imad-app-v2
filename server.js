c15600416e53ffbe8a041e1aad43debb850b4df9
inderjeet kaur khokhar <shikwanti@gmail.com>
2017-03-22 21:23:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -156,15 +156,24 @@ app.get('/articles/:articleName',function(req,res)
     });
    
 });
+
+
+
+
+
 function hash(input,salt)
 {
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512' );
-    return hashed;
+    return hashed.toString('hex');
 }
+
 app.get('hash/:input',function(req,res){
    var hashedString = hash(req.params.input,'this-is-a-random-string');
    res.send(hashedString);
 });
+
+
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
