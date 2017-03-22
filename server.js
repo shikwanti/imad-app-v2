28164d0571fbde17cbb7c86163916171b8216b1b
inderjeet kaur khokhar <shikwanti@gmail.com>
2017-03-22 21:26:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ function hash(input,salt)
     return hashed.toString('hex');
 }
 
-app.get('hash/:input',function(req,res){
+app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input,'this-is-a-random-string');
    res.send(hashedString);
 });
